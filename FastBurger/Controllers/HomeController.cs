8f9453d67b0d0271f430e36be9ffa6bd8c6aba04
Felipe Muniz <lipe.baterra@gmail.com>
2022-08-10 21:38:20 -0300

    Implementando a View do Carrinho de compra e implementamos na home os Lanches preferidos da semana

--- FastBurger/Controllers/HomeController.cs
+++ FastBurger/Controllers/HomeController.cs
@@ -1,14 +1,26 @@
 ﻿using FastBurger.Models;
+using FastBurger.Repository.Interfaces;
+using FastBurger.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace FastBurger.Controllers
 {
     public class HomeController : Controller
-    {       
+    {
+        private readonly ILancheRepository _lancheRepository;
+public HomeController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ FastBurger/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using FastBurger.Models;
+
+namespace FastBurger.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+    }
+}
