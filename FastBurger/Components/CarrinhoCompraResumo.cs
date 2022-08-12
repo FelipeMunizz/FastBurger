8c38ef292bfa292fbf3a033de7fec589d8959a60
Felipe Muniz <lipe.baterra@gmail.com>
2022-08-11 21:59:30 -0300

    Criando uma ViewComponent para exibir detalhes do carrinho

---
+++ FastBurger/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,31 @@
+﻿using FastBurger.Models;
+using FastBurger.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastBurger.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraVW = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVW);
+        }
+    }
+}
