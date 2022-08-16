4e8725a6c8b653c7ecb3a2d1984f7ccc5a7554a7
Felipe Muniz <lipe.baterra@gmail.com>
2022-08-16 14:56:19 -0300

    Implementamos a TagHelper para Contato

---
+++ FastBurger/Components/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FastBurger.Components
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
