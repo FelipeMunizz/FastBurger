7bedfee0e597d92dfe5626b97c9a8ff555181880
Felipe Muniz <lipe.baterra@gmail.com>
2022-08-17 15:53:40 -0300

    Criamos uma view components para exibir as categorias no menu produto

---
+++ FastBurger/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using FastBurger.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastBurger.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _repository;
+
+        public CategoriaMenu(ICategoriaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _repository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
--- FastBurger/Program.cs
+++ FastBurger/Program.cs
@@ -42,6 +42,10 @@ app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "categoriaFiltro",
+    pattern: "Lanche/{action}/{categoria?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
