0dc3b725cc1562c3c43b64aabbfbae021e686e2d
Felipe Muniz <lipe.baterra@gmail.com>
2022-10-11 23:08:37 -0300

    Checkout

---
+++ FastBurger/Components/PedidoController.cs
@@ -0,0 +1,75 @@
+﻿using FastBurger.Models;
+using FastBurger.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastBurger.Components
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0;
+
+            #region Obtem os itens do carrinho de compra do cliente
+
+            //obtem os itens do carrinho de compra do cliente
+            List<CarrinhoCompraItem> itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            //verifica se existem itens de pedido
+            if(_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Carrinho Vazio");
+            }
+
+            //calcula o total de intens e o total do pedido
+            foreach(var item in itens)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += item.Lanche.Preco * item.Quantidade;
+            }
+
+            //atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+            #endregion
+
+            #region Criação do Pedido
+
+            //valida os dados do pedido
+            if (ModelState.IsValid)
+            {
+                //cria o pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //define as mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //limpa o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //exibe a view com dados do cliente e do pedido
+                return View("~/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
+            #endregion
+        }
+    }
+}
